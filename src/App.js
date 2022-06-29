8d34eb391f2a9a9a690132504b66649cf4700d55
Ishara Madhushanka <ishara.madhushanka@du.ae>
2022-06-29 14:35:46 +0530

    setting up global exceptio handler

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import type {Node} from 'react';
+import GlobalExceptionHandler from './components/organism/GlobalExceptionHandler';
 import Routes from './routes';
 
 const App: () => Node = () => {
-  return <Routes />;
+  return (
+    <>
+      <GlobalExceptionHandler />
+      <Routes />
+    </>
+  );
 };
 
 export default App;
