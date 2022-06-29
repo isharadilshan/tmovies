bd4030036268b9ed9ff0e2d731a53af258f6f711
Ishara Dilshan <isharadilshandev@gmail.com>
2022-06-29 12:14:41 +0530

    setting up eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
   root: true,
+  plugins: ['import'],
   extends: '@react-native-community',
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'prefer-const': 1,
+    'react/no-array-index-key': [1],
+    'no-console': 'warn',
+    'react/prop-types': 1,
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
