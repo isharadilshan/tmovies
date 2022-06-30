da9adc0a0dc39e06f680fceda3142fdd40821a00
Ishara Dilshan <isharadilshandev@gmail.com>
2022-06-30 19:01:30 +0530

    added environment support

--- src/services/movies/index.js
+++ src/services/movies/index.js
@@ -1,6 +1,7 @@
+import Config from 'react-native-config';
 import {TOP_RATED_MOVIES} from '../endpoints';
 import {Axios} from '../rest-client';
 
 export const getTopRatedMovies = page => {
-  return Axios.get(TOP_RATED_MOVIES('9618b5cf6ae9661f92fff553c697bed4', page));
+  return Axios.get(TOP_RATED_MOVIES(Config.API_KEY, page));
 };
